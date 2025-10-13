52b9e0c076b66bfb57a87c444cff80febf3d3ee1
HADI-ITHÖGSKOLA\hadid <hadi.daliri@iths.se>
2025-10-13 14:09:38 +0200

    Snake movement improved

--- Lab_02/GameObjects/Enemies/Snake.cs
+++ Lab_02/GameObjects/Enemies/Snake.cs
@@ -116,11 +116,91 @@ namespace Lab_02.GameObjects.Enemies
 
             if (IsVisible)
             {
-                var chosenDirection = new Random();
+                int chosenDirection;
+                var randomDirection = new Random();
                 double distanceToPlayer;
                 distanceToPlayer = Math.Round(Math.Sqrt(Math.Abs(player.positionY - positionY) * Math.Abs(player.positionY - positionY) + Math.Abs(player.positionX - positionX) * Math.Abs(player.positionX - positionX)));
-                if (distanceToPlayer < 3)
+                if (distanceToPlayer < 2)
                 {
+                    switch (input)
+                    {
+                        case 'a':
+                        case 'A':
+                            chosenDirection = randomDirection.Next(1, 4);
+                            if (chosenDirection == 1)
+                            {
+                                MoveRight('d');
+                                break;
+                            }
+                            else if (chosenDirection == 2)
+                            {
+                                MoveUp('w');
+                                break;
+                            }
+                            else
+                            {
+                                MoveDown('s');
+                                break;
+                            }
+                        case 'd':
+                        case 'D':
+                            chosenDirection = randomDirection.Next(1, 4);
+                            if (chosenDirection == 1)
+                            {
+                                MoveLeft('a');
+                                break;
+                            }
+                            else if (chosenDirection == 2)
+                            {
+                                MoveUp('w');
+                                break;
+                            }
+                            else
+                            {
+                                MoveDown('s');
+                                break;
+                            }
+                        case 'w':
+                        case 'W':
+                            chosenDirection = randomDirection.Next(1, 4);
+                            if (chosenDirection == 1)
+                            {
+                                MoveRight('d');
+                                break;
+                            }
+                            else if (chosenDirection == 2)
+                            {
+                                MoveLeft('a');
+                                break;
+                            }
+                            else
+                            {
+                                MoveDown('s');
+                                break;
+                            }
+                        case 's':
+                        case 'S':
+                            chosenDirection = randomDirection.Next(1, 4);
+                            if (chosenDirection == 1)
+                            {
+                                MoveRight('d');
+                                break;
+                            }
+                            else if (chosenDirection == 2)
+                            {
+                                MoveUp('w');
+                                break;
+                            }
+                            else
+                            {
+                                MoveLeft('a');
+                                break;
+                            }
+                        default:
+                            break;
+                    }
+                }
+                else if (distanceToPlayer < 3)
                     switch (input)
                     {
                         case 'a':
@@ -131,19 +211,18 @@ namespace Lab_02.GameObjects.Enemies
                         case 'D':
                             MoveRight(input);
                             break;
-                        case 'w':
-                        case 'W':
-                            MoveUp(input);
-                            break;
                         case 's':
                         case 'S':
                             MoveDown(input);
                             break;
+                        case 'w':
+                        case 'W':
+                            MoveUp(input);
+                            break;
                         default:
                             break;
-
                     }
-                }
+
             }
         }
     }
